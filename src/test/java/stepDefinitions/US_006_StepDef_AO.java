e4149a8db4e21e41f08e0904a1db444ef98dba32
gurhantugan <95536059+gurhantugan@users.noreply.github.com>
2023-02-05 11:25:34 +0000

    uso12

--- src/test/java/stepDefinitions/US_006_StepDef_AO.java
+++ src/test/java/stepDefinitions/US_006_StepDef_AO.java
@@ -33,4 +33,6 @@ public class US_006_StepDef_AO {
 
 
     }
+
+
 }
---
+++ src/test/java/stepDefinitions/US_012_StepDef_GT.java
@@ -0,0 +1,14 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US_012_StepDef_GT {
+    @Then("user verifies that SELL, SHARE, TRADE buttons are clickable in the Sell Your Products area")
+    public void userVerifiesThatSELLSHARETRADEButtonsAreClickableInTheSellYourProductsArea() {
+    }
+
+    @When("user clicks on SELL, SHARE, TRADE buttons then verifies that relevant pages are opened")
+    public void userClicksOnSELLSHARETRADEButtonsThenVerifiesThatRelevantPagesAreOpened() {
+    }
+}
