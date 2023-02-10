be6cf197afb37de305aa1aa060c41bfd0bbeac3c
haka1453 <huseyinaka01@gmail.com>
2023-02-10 23:20:07 +0000

    control

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "stepDefinitions",
 
 
-        tags = "@abd_25",
+        tags = "@US_001_HA",
         dryRun = false
 )
 public class CukesRunner {
