fe4dd36e0cd8279e0bcda091cb63d7711d2d6943
gurhantugan <95536059+gurhantugan@users.noreply.github.com>
2023-02-14 17:33:28 +0000

    aaa

--- src/test/java/stepDefinitions/US_036_StepDef_AK.java
+++ src/test/java/stepDefinitions/US_036_StepDef_AK.java
@@ -12,37 +12,37 @@ package stepDefinitions;
 //
 public class US_036_StepDef_AK { 
 
- LoginPage loginPage = new LoginPage();
-
- @When("user clicks login button")
-public void userClicksLoginButton() {
- BrowserUtilities.waitForVisibility(loginPage.button_login,20);
-  BrowserUtilities.clickWithJS(loginPage.button_login);
- }
-
-    @Then("User verifies that the mail text box is clickable")
-    public void userVerifiesThatTheMailTextBoxIsClickable() {
-        assertTrue(loginPage.textbox_email.isEnabled());
-    }
-
-   @Then("Verifies that the user password text box is clickable")
-    public void verifiesThatTheUserPasswordTextBoxIsClickable() {
-        assertTrue(loginPage.button_passwordOnLoginPage.isEnabled());
-   }
-
-    @Given("User enters values {string} in the email text box and {string} in the password text box")
-    public void userEntersValuesInTheEmailTextBoxAndInThePasswordTextBox(String invalidEmail, String invaildPassword) {
-        loginPage.textbox_email.sendKeys(invalidEmail);
-        loginPage.button_passwordOnLoginPage.sendKeys(invaildPassword);
-   }
-   
-    @Then("User verifies unable to login with {string}")
-    public void userVerifiesUnableToLoginWith(String expectedValue) {
-        BrowserUtilities.waitFor(2);
-        loginPage.button_forgotPassword.click();
-        BrowserUtilities.waitFor(2);
-
-        Driver.closeDriver();
+// LoginPage loginPage = new LoginPage();
+//
+// @When("user clicks login button")
+//public void userClicksLoginButton() {
+// BrowserUtilities.waitForVisibility(loginPage.button_login,20);
+//  BrowserUtilities.clickWithJS(loginPage.button_login);
+// }
+//
+//    @Then("User verifies that the mail text box is clickable")
+//    public void userVerifiesThatTheMailTextBoxIsClickable() {
+//        assertTrue(loginPage.textbox_email.isEnabled());
+//    }
+//
+//   @Then("Verifies that the user password text box is clickable")
+//    public void verifiesThatTheUserPasswordTextBoxIsClickable() {
+//        assertTrue(loginPage.button_passwordOnLoginPage.isEnabled());
+//   }
+//
+//    @Given("User enters values {string} in the email text box and {string} in the password text box")
+//    public void userEntersValuesInTheEmailTextBoxAndInThePasswordTextBox(String invalidEmail, String invaildPassword) {
+//        loginPage.textbox_email.sendKeys(invalidEmail);
+//        loginPage.button_passwordOnLoginPage.sendKeys(invaildPassword);
+//   }
+//
+//    @Then("User verifies unable to login with {string}")
+//    public void userVerifiesUnableToLoginWith(String expectedValue) {
+//        BrowserUtilities.waitFor(2);
+//        loginPage.button_forgotPassword.click();
+//        BrowserUtilities.waitFor(2);
+//
+//        Driver.closeDriver();
     }
 
 //    LoginPage loginPage = new LoginPage();
@@ -79,4 +79,4 @@ public void userClicksLoginButton() {
 //        Driver.closeDriver();
 //    }
 
-}
+
