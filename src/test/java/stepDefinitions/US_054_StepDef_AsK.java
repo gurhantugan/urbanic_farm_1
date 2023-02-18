25b71cabd74a9f19889781f52bad3b0a48590137
Aslan <aslankartal1982@gmail.com>
2023-02-18 17:46:01 +0000

    first saturday

---
+++ src/test/java/stepDefinitions/US_054_StepDef_AsK.java
@@ -0,0 +1,60 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US_054_StepDef_AsK {@When("user clicks slider button")
+public void user_clicks_slider_button() {
+
+}
+
+    @When("user picks up on the vine")
+    public void user_picks_up_on_the_vine() {
+
+    }
+
+    @Then("user picks up on the vine checkbox should be clickable")
+    public void user_picks_up_on_the_vine_checkbox_should_be_clickable() {
+
+    }
+
+    @When("user while clicking picks up on the vine checkbox should show {string} menu functional and visible")
+    public void user_while_clicking_picks_up_on_the_vine_checkbox_should_show_menu_functional_and_visible(String string) {
+
+    }
+
+    @When("user chose same hours \"Start time cannot be greater than end time at the Buyer picks up on the vine field.\"massage")
+    public void user_chose_same_hours_start_time_cannot_be_greater_than_end_time_at_the_buyer_picks_up_on_the_vine_field_massage() {
+
+    }
+
+    @Then("Your available hours is clicked, suitable hours should be selected from the menu")
+    public void your_available_hours_is_clicked_suitable_hours_should_be_selected_from_the_menu() {
+
+    }
+
+    @Then("user clicks search button")
+    public void userClicksSearchButton() {
+    }
+
+    @And("user click {string} button and click")
+    public void userClickButtonAndClick(String arg0) {
+    }
+
+    @And("user click delivery and pick up setting")
+    public void userClickDeliveryAndPickUpSetting() {
+    }
+
+    @When("user chose same hours and click update button, {string}massage")
+    public void userChoseSameHoursAndClickUpdateButtonMassage(String arg0) {
+    }
+
+    @Then("Your available hours is clicked and update button, suitable hours should be selected from the menu")
+    public void yourAvailableHoursIsClickedAndUpdateButtonSuitableHoursShouldBeSelectedFromTheMenu() {
+    }
+
+    @Then("Your available hours is clicked and update button, {string} alert should be selected from the menu")
+    public void yourAvailableHoursIsClickedAndUpdateButtonAlertShouldBeSelectedFromTheMenu(String arg0) {
+    }
+}
