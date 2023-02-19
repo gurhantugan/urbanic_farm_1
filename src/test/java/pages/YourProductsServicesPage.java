b4e7e73a59d7b9d74e82860f0a5a60c1fe66f2fc
aeski08 <60294078+aeski08@users.noreply.github.com>
2023-02-19 00:36:35 +0000

    060Finished

--- src/test/java/pages/YourProductsServicesPage.java
+++ src/test/java/pages/YourProductsServicesPage.java
@@ -1,4 +1,15 @@
 package pages;
 
-public class YourProductsServicesPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class YourProductsServicesPage extends CommonPage{
+    @FindBy(css = ".col-6.col-sm-4.text-center.mb-3.cursor-pointer.text-secondary")
+    public List<WebElement> links_allProducts;
+
+    @FindBy(css = ".GoBack_goback__6sa4O")
+    public WebElement button_goBack;
+
 }
