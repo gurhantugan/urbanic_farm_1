c42c5f884e478f8418f711b5c8d2201c3769dd98
gokceeksioglu <gokceeksioglu@hotmail.com>
2023-02-21 11:37:26 +0000

    us063

--- src/test/java/stepDefinitions/US_063_StepDef_GK.java
+++ src/test/java/stepDefinitions/US_063_StepDef_GK.java
@@ -3,18 +3,12 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import pages.AccountPage;
-import pages.HomePage;
 import pages.LoginPage;
-import pages.WelcomePage;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 
-import java.util.NavigableMap;
-
 public class US_063_StepDef_GK {
 
-     WelcomePage welcomePage=new WelcomePage();
      LoginPage loginPage=new LoginPage();
 
     @Given("User enter to Home Page")
@@ -27,19 +21,19 @@ public class US_063_StepDef_GK {
     @When("User see and click the login button")
     public void user_see_and_click_the_login_button() {
 
-    welcomePage.loginButton.click();
+
     }
 
 
 
     @Then("User enters buyer valid email address")
     public void user_enters_buyer_valid_email_address() {
-        welcomePage.buyerEmail.sendKeys(ConfigurationReader.getProperty("buyerEmail"));
+
 
     }
     @Then("User enters buyer valid password")
     public void user_enters_buyer_valid_password() {
-      welcomePage.buyerPassword.sendKeys(ConfigurationReader.getProperty("buyerPassword"));
+
 
     }
 
