10fd6cb7f892ae5803627fa834e370799bbace44
aeski08 <60294078+aeski08@users.noreply.github.com>
2023-02-22 23:46:23 +0000

    066

--- src/test/java/stepDefinitions/US_066_StepDef_AO.java
+++ src/test/java/stepDefinitions/US_066_StepDef_AO.java
@@ -16,8 +16,11 @@ public class US_066_StepDef_AO {
 
     CardAndBasketPage cardAndBasketPage = new CardAndBasketPage();
     Random random = new Random();
-    int quantityBefore;
-    int quantityAfter;
+    int  quantityBefore;
+    int  quantityAfter;
+    double totalCostBefore;
+    double totalCostAfter;
+    double price;
 
     @Given("user goes to {string} page after login as a buyer")
     public void userGoesToPageAfterLoginAsABuyer(String endpoint) {
@@ -35,30 +38,49 @@ public class US_066_StepDef_AO {
 
     @When("user clicks on plus button")
     public void userClicksOnPlusButton() {
-        quantityBefore = Integer.parseInt(cardAndBasketPage.number_quantity.get(1).getText());
+        quantityBefore = Integer.parseInt(cardAndBasketPage.number_quantity.get(0).getAttribute("value"));
+        System.out.println("quantityBefore = " + quantityBefore);
+        totalCostBefore = Double.parseDouble(cardAndBasketPage.totalCost.getText().substring(1).replace(".",""))/100;
+        System.out.println("totalCostBefore = " + totalCostBefore);
+
         JSUtils.clickElementByJS(cardAndBasketPage.button_plus);
         BrowserUtilities.waitFor(2);
-
     }
 
     @And("quantity should be increased")
     public void quantityShouldBeIncreased() {
-        quantityAfter = Integer.parseInt(cardAndBasketPage.number_quantity.get(1).getText());
+        quantityAfter = Integer.parseInt(cardAndBasketPage.number_quantity.get(0).getAttribute("value"));
+        System.out.println("quantityAfter = " + quantityAfter);
         BrowserUtilities.waitFor(2);
         Assert.assertEquals(quantityBefore+1,quantityAfter);
+
+        price = Double.parseDouble(cardAndBasketPage.price.getText().substring(1).replace(".",""))/100;
+        System.out.println("price = " + price);
+        totalCostAfter = Double.parseDouble(cardAndBasketPage.totalCost.getText().substring(1).replace(".",""))/100;
+        System.out.println("totalCostAfter = " + totalCostAfter);
+
+
 
     }
 
     @Then("user should see total correct results in product increases")
     public void userShouldSeeTotalCorrectResultsInProductIncreases() {
+        try {
+            Assert.assertTrue(totalCostBefore+price == totalCostAfter);
+        } catch (Exception exception){
+            System.out.println(totalCostBefore+price+"="+totalCostAfter);
+            System.out.println("There is a mistake in assertion");
+        }
     }
 
     @When("user clicks on delete button")
     public void userClicksOnDeleteButton() {
+        cardAndBasketPage.button_delete.click();
     }
 
     @Then("user should see message {string}")
     public void userShouldSeeMessage(String arg0) {
+        Assert.assertTrue(cardAndBasketPage.text.isDisplayed());
     }
 
     @Then("user verifies the item deleted from cart")
