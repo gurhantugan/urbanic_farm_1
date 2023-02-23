8bae40f75981245b5ef2a13b9feca4305328c313
gbahadir <93818960+gbahadir@users.noreply.github.com>
2023-02-23 14:17:33 +0000

    new

--- src/test/java/stepDefinitions/US_036_StepDef_AK.java
+++ src/test/java/stepDefinitions/US_036_StepDef_AK.java
@@ -58,22 +58,6 @@ public class US_036_StepDef_AK {
 
        }*/
 
-    @Given("User enters values {string} in the email text box and {string} in the password text box")
-    public void userEntersValuesInTheEmailTextBoxAndInThePasswordTextBox(String invalidEmail, String invalidPassword) {
-        loginPage.textbox_email.sendKeys(invalidEmail);
-        loginPage.textbox_password.sendKeys(invalidPassword);
-    }
-    @Then("User verifies unable to login with {string}")
-    public void userVerifiesUnableToLoginWith(String expectedValue) {
-        BrowserUtilities.waitFor(2);
-        loginPage.button_forgotPassword.click();
-        BrowserUtilities.waitFor(2);
-        assertTrue(loginPage.message_pleaseVerifyYourEmail.isDisplayed());
-        BrowserUtilities.waitFor(10);
-        Driver.closeDriver();
-
-    }*/
-
     @Then("Verifies that the user password text box is clickable")
     public void verifiesThatTheUserPasswordTextBoxIsClickable() {
         assertTrue(loginPage.button_passwordOnLoginPage.isEnabled());
