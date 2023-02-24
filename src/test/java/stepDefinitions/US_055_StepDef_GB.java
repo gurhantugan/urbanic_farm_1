09afb4ebb7d4ad7fedbd705f3fb65c877eae76c3
gbahadir <93818960+gbahadir@users.noreply.github.com>
2023-02-24 21:28:08 +0000

    2

--- src/test/java/stepDefinitions/US_055_StepDef_GB.java
+++ src/test/java/stepDefinitions/US_055_StepDef_GB.java
@@ -5,12 +5,4 @@ import utilities.BrowserUtilities;
 import utilities.ConfigurationReader;
 
 public class US_055_StepDef_GB {
-    @Given("user goes to welcome page after login")
-    public void userGoesToWelcomePageAfterLogin() {
-        BrowserUtilities.loginWithTokenBuyer(ConfigurationReader.getProperty("tokenBuyerUrl"));
-    }
 }
-   /* @Given("user goes to {string} page after login")
-    public void userGoesToPageAfterLogin(String endPoint) {
-        BrowserUtilities.loginWithTokenSeller(ConfigurationReader.getProperty("tokenSellerUrl"),endPoint);
-        BrowserUtilities.waitFor(2);*/
