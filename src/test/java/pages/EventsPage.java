dd370d0ec95158db1e0229a53661e44a39eca9be
Abdullah Kadioglu <abdkadioglu21@gmail.com>
2023-03-03 00:44:26 +0300

    US75

--- src/test/java/pages/EventsPage.java
+++ src/test/java/pages/EventsPage.java
@@ -5,9 +5,6 @@ import org.openqa.selenium.support.FindBy;
 
 public class EventsPage extends CommonPage{
 
-    @FindBy(css = "class=\"Navbar_button__9mlDv\"")
-    public WebElement button_Navbar;
-
     @FindBy(xpath = "(//*[@href=\"/account/events\"])[2]")
      public WebElement button_events;
 
