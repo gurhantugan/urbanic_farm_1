d16c65a501f067e51cac00e77693fdd78dbc58ce
Elif Deniz <94469409+ElifSena55@users.noreply.github.com>
2023-03-07 13:33:38 +0300

    072

--- src/test/java/pages/MyEventsPage.java
+++ src/test/java/pages/MyEventsPage.java
@@ -10,4 +10,34 @@ public class MyEventsPage extends CommonPage {
 
     @FindBy(xpath = "//button[@class='events-i-organize_btnCreate__2AWbt']")
     public WebElement button_Create_newEvents;
+
+    @FindBy(css="#title")
+    public WebElement box_title;
+
+    @FindBy(css="#address2")
+    public WebElement box_Address;
+
+    @FindBy(css="#date")
+    public WebElement box_Date;
+
+    @FindBy(css="#time")
+    public WebElement box_time;
+
+    @FindBy(css="#fee")
+    public WebElement box_Fee;
+
+    @FindBy(css="#duration")
+    public WebElement box_duration;
+
+    @FindBy(css="#attendeeLimit")
+    public WebElement box_attendeeLimit;
+
+    @FindBy(css="#tac")
+    public WebElement box_TermsAndConditions;
+
+    @FindBy(css=".btn.btn-outline-success.px-4")
+    public WebElement button_Submit;
+
+
+
 }
