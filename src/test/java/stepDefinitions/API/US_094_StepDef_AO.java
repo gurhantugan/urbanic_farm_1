0f42451293b82965aa82cb39c44e6160144731b2
gurhantugan <95536059+gurhantugan@users.noreply.github.com>
2023-03-08 12:43:49 +0000

    abc

--- src/test/java/stepDefinitions/API/US_094_StepDef_AO.java
+++ src/test/java/stepDefinitions/API/US_094_StepDef_AO.java
@@ -1,6 +1,9 @@
 package stepDefinitions.API;
 
+import enums.USER;
 import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 import static utilities.ApiUtilities.requestSpecification;
@@ -8,11 +11,12 @@ import static utilities.ApiUtilities.response;
 
 public class US_094_StepDef_AO {
     String token;
+    Response response;
 
     @Then("User gets all existing hubs' information")
     public void userGetsAllExistingHubsInformation() {
 
-        response = given().spec(requestSpecification(token)).post("account/hub/getMyHubs");
+        response = given().contentType(ContentType.JSON).spec(requestSpecification(USER.SELLER.getToken())).post("/account/hub/getMyHubs");
         response.prettyPrint();
     }
 }
