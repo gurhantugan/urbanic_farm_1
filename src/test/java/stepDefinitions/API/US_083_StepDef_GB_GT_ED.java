d63076f879799f92bec0484db1537bf91bd5ba8f
gurhantugan <95536059+gurhantugan@users.noreply.github.com>
2023-03-14 14:50:49 +0000

    us83,84,98

--- src/test/java/stepDefinitions/API/US_083_StepDef_GB_GT_ED.java
+++ src/test/java/stepDefinitions/API/US_083_StepDef_GB_GT_ED.java
@@ -1,6 +1,7 @@
 package stepDefinitions.API;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -11,15 +12,34 @@ import static io.restassured.RestAssured.given;
 import static utilities.ApiUtilities.*;
 
 import io.cucumber.datatable.DataTable;
+import org.junit.Assert;
 
 public class US_083_StepDef_GB_GT_ED {
 
     Response response;
 
+    @Given("user connects to the {string} with payload")
+    public void userConnectsToTheWithPayload(String endpoint, DataTable dataTable) {
+
+        Map<String, String> payload  = dataTable.asMaps().get(0);
+
+        System.out.println("payload = " + payload);
+
+        response = given().contentType(ContentType.JSON).spec(requestSpecification2())
+                .body(payload).post(endpoint);
+        response.prettyPrint();
+        System.out.println(response.getStatusCode());
+    }
+
+
 //    @Given("user connects to the {string} with payload")
-//    public void userConnectsToTheWithPayload(String endpoint, DataTable dataTable) {
+//    public void userConnectsToTheWithPayload(String endpoint) {
 //
-//        Map<String, String> git  = dataTable.asMap();
+//        Map<String, String> payload = new HashMap<>();
+//        payload.put("email", "abc@def.com");
+//        payload.put("message", "Hello world");
+//        payload.put("name", "John Smith");
+//        payload.put("subject", "Tomato");
 //
 //        System.out.println("payload = " + payload);
 //
@@ -29,28 +49,9 @@ public class US_083_StepDef_GB_GT_ED {
 //        System.out.println(response.getStatusCode());
 //    }
 
-
-
-
-
-
-
-
-    @Given("user connects to the {string} with payload")
-    public void userConnectsToTheWithPayload(String endpoint) {
-
-        Map<String, String> payload = new HashMap<>();
-        payload.put("email", "abc@def.com");
-        payload.put("message", "Hello world");
-        payload.put("name", "John Smith");
-        payload.put("subject", "Tomato");
-
-        System.out.println("payload = " + payload);
-
-        response = given().contentType(ContentType.JSON).spec(requestSpecification2())
-                .body(payload).post(endpoint);
-        response.prettyPrint();
-        System.out.println(response.getStatusCode());
+    @Then("user verifies that the response is expectedd")
+    public void userVerifiesThatTheResponseIsExpectedd() {
+        Assert.assertTrue("true", response.jsonPath().getBoolean("success"));
     }
 
 }
--- src/test/resources/features.API_features/US_098_MO_SG.feature
+++ src/test/resources/features.API_features/US_098_MO_SG.feature
@@ -4,9 +4,9 @@ Feature: As a user, I should be able to add a product to the hub
     Given user logs in with api
     Then  usser adds a product
     Then  user verifies the status code is 200
-   # Then user verifies that the response is as expected
+   Then user verifies that the response is as expectedd
       | success | true                        |
-      | product | VEGETABLES_AND_FRUITS_Basil |
+#      | product | VEGETABLES_AND_FRUITS_Basil |
 
 
 
