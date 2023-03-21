fdd32ce8d5346450d1756656d3a53e30fb79942a
gurhantugan <95536059+gurhantugan@users.noreply.github.com>
2023-03-21 13:51:05 +0000

    us109

--- src/test/java/stepDefinitions/API/US_110_StepDef_AO_GT.java
+++ src/test/java/stepDefinitions/API/US_110_StepDef_AO_GT.java
@@ -13,14 +13,21 @@ public class US_110_StepDef_AO_GT {
     public void userVerifiesSomeAttendanceTitlesAreAsFollows(DataTable dataTable) {
 
         List<String> expectedAttendanceTitles=dataTable.column(0);
-        List<String> allActualAttendanceTitles =response.jsonPath().getList("attendances.event.title");
-        for (int i = 0; i < allActualAttendanceTitles.size(); i++) {
-            System.out.println("title"+(i+1)+"= "+  allActualAttendanceTitles.get(i).toString());
-           // Assert.assertEquals(allExpextedTitles.get(i).toString(), allActualAttendanceTitles.get(i).toString());
+        System.out.println("expectedAttendanceTitles = " + expectedAttendanceTitles);
+       // List<String> allActualAttendanceTitles =response.jsonPath().getList("attendances.event.title");
+        String successMessage=response.jsonPath().getString("success");
+        System.out.println("successMessage = " + successMessage);
+
+
+
+
+//        for (int i = 0; i < allActualAttendanceTitles.size(); i++) {
+//            System.out.println( allActualAttendanceTitles.get(i).toString());
+//           // Assert.assertEquals(allExpextedTitles.get(i).toString(), allActualAttendanceTitles.get(i).toString());
 
 //Assert.assertTrue(cp.genders.stream().anyMatch(t -> t.getText().equals("Male")));
 
         }
     }
-}
+
 
